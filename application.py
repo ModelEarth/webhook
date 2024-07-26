1b84541441371ca13d6f031b005936309490178e
Nora Tang <jingyu_tang@brown.edu>
2024-07-26 12:13:48 -0500

    passed simple local tests

--- application.py
+++ application.py
@@ -1,25 +1,46 @@
 from flask import Flask, request, jsonify
 from docx import Document
 import os
+import re
 
 application = Flask(__name__)
 
-@application.route("/")
-def test_page():
-    return "This is a test page!"
+placeholders={
+    '#startDate#': '26b40600',
+    '#schoolDegree#': '11c2cd54', #optional
+    '#degreeDate#': '0b03fd30', #optional
+    '#name#': '6a2f8ab5',
+    '#optDepartment#': '01bcc04b', #optional
+    '#optContact#': '3ab50c1d', #optional
+    '#workingHours#': '0c8671f9',
+    '#github#': '51a6d3df',
+    '#email#': '61c73e81',
+    '#endDate#': '31e5f166', #optional
+    '#phone#': '086a67d6' #optional
+}
 
-@application.route('/webhook', methods=['POST'])
+@application.route('/signup', methods=['POST'])
 def webhook():
     data = request.json
-    doc = Document()
-    doc.add_heading('Form Response', level=1)
+    doc = Document('offer_letter_template.docx')
 
-    # Extracting answers
+    # extract answers
+    answers = {}
     for question_id, answer_data in data['answers'].items():
-        question_answers = [answer['value'] for answer in answer_data['textAnswers']['answers']]
-        doc.add_paragraph(f"Question ID {question_id}: {', '.join(question_answers)}")
+        answers[question_id] = [answer['value'] for answer in answer_data['textAnswers']['answers']]
 
-    doc_path = os.path.join('generated_docs', 'FormData.docx')
+    # replace placeholders
+    pattern = re.compile(r'#\w+#')
+    for paragraph in doc.paragraphs:
+        for match in re.findall(pattern, paragraph.text):
+            if match in placeholders:
+                # check optional fields in the form
+                to_replace = ''
+                if placeholders[match] in answers:
+                    to_replace = answers[placeholders[match]][0]
+                paragraph.text = paragraph.text.replace(match, to_replace)
+  
+    doc_path = os.path.join('generated_docs', f"{answers['6a2f8ab5'][0]}_Offer_Letter.docx")
     if not os.path.exists('generated_docs'):
         os.makedirs('generated_docs')
     doc.save(doc_path)
